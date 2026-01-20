4024091cfc05adbef2195b1360f88f166c771262
killux <mustaphaboutoubdev@gmail.com>
2026-01-20 12:44:04 +0100

    ls

--- custom-ls.sh
+++ custom-ls.sh
@@ -1,2 +1,1 @@
-alias custom-ls='ls -lasS | awk "{if(NR==1){c1=length(\$1);c6=length(\$6)} printf \"%*s %s %s %s %*s %s %s %s %s\\n\", c1, \$1, \$2, \$3, \$4, c6, \$6, \$7, \$8, \$9, \$NF}"'
-
+alias='ls -lasS'
