25c17b9c05ba6d932a9cbb5d79aa93fbd0be9f5b
killux <mustaphaboutoubdev@gmail.com>
2026-01-21 10:54:09 +0100

    test command

--- hard-conditions.sh
+++ hard-conditions.sh
@@ -1,1 +1,1 @@
-test [ -x $1 ] && echo "File is executable" || echo "File is not an executable or does not exist"
+[ -x $1 ] && echo "File is executable" || echo "File is not an executable or does not exist"
