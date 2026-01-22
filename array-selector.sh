044a182932981670bd8886c159b6517fd43041eb
killux <mustaphaboutoubdev@gmail.com>
2026-01-22 12:04:55 +0100

    array

--- array-selector.sh
+++ array-selector.sh
@@ -4,10 +4,8 @@ arr=("red" "blue" "green" "white" "black")
 
 if [[ $# != 1 ]] || [[ ! $1 =~ ^[+]?[1-5]$ ]] ; then
 		echo "Error"
-		exit 1
+else 
+		index=$(expr $1 - 1)
+        echo ${arr[$index]}
 fi
-
-index=$(expr $1 - 1)
-echo ${arr[$index]}
-	
 	
