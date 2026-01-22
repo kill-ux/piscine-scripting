a1d77d8e773c9460419e848449cfe97f5de81cf8
killux <mustaphaboutoubdev@gmail.com>
2026-01-22 10:46:23 +0100

    division

--- division.sh
+++ division.sh
@@ -3,8 +3,8 @@ if [[ $1 == "" ]] || [[ $2 == "" ]]; then
 		echo "Error: two numbers must be provided"
 elif [[ ! $1 =~ ^[+-]?[0-9]+$ ]] || [[ ! $2 =~ ^[+-]?[0-9]+$ ]]; then
 		echo "Error: both arguments must be integers"
-elif [[ $2 == "0"]] 
+elif [[ $2 == 0 ]]; then 
 		echo "Error: division by zero is not allowed"
 else
-	   echo "$1 / $2" | bc
+	    echo "$1 / $2" | bc
 fi
