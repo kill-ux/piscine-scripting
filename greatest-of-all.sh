e0a0cf1e32fb7d19787c994aa7499c4678579134
killux <mustaphaboutoubdev@gmail.com>
2026-01-22 12:44:55 +0100

    ark

---
+++ greatest-of-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+max=0
+for i in {0..10}; do
+		read -p "Enter a number: " num
+		if ! [[ $num =~ ^[0-9]+$ ]]; then
+				echo "ERROR: Invalid input only positive numerical characters are allowed"
+				exit 1
+		elif [[ $num -gt 10000 ]]; then
+				echo "ERROR: The number entered is too large"
+				exit 1
+		fi
+		if [[ $num -gt $max ]]; then
+				max=$num
+		fi
+done
+
+echo "The largest number is: $max"
