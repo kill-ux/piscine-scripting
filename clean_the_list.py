ad80c901212967b5329adc6fe089305e9933b6ad
killux <mustaphaboutoubdev@gmail.com>
2026-01-26 11:23:24 +0100

    lists

--- clean_the_list.py
+++ clean_the_list.py
@@ -1,6 +1,8 @@
 
 def clean_list(list):
+    if 'milk' not in list and len(list) != 0:
+        list.append('milk')
     for i,item in enumerate(list) :
-        list[i] = f"{i}/ {item.strip().capitalize()}"
+        list[i] = f"{i+1}/ {item.strip().capitalize()}"
     return list
     
