238504ef63a649683467cfefe6570f00b7020290
killux <mustaphaboutoubdev@gmail.com>
2026-01-27 11:04:08 +0100

    days

--- merge_two.py
+++ merge_two.py
@@ -3,9 +3,11 @@ import json
 def merge_two(map1):
     map2 = {}
     while True:
+        print("key: ", end="")
         key = input()
         if key == "exit":
             break
+        print("value: ", end="")
         value = input()
         map2[key] = int(value)
     map = map1 | map2
