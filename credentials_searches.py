4f63fdc640d075dba16ebbbbc6d5ba5a16e72373
killux <mustaphaboutoubdev@gmail.com>
2026-01-27 13:31:31 +0100

    parse json and wwirte it in file

--- credentials_searches.py
+++ credentials_searches.py
@@ -2,14 +2,17 @@ import json, os
 
 def credentials_search():
     if os.access("logs.json", os.R_OK):
-        with open("logs.json") as fd_log:
-            data = json.load(fd_log)
-            res = {}
-            if isinstance(data, dict):
-                res = read_logs(data)
-            if res:
-                with open("credentials.json", "w") as fd_cr:
-                    fd_cr.write(json.dumps(res))
+        try:
+            with open("logs.json") as fd_log:
+                data = json.load(fd_log)
+                res = {}
+                if isinstance(data, dict):
+                    res = read_logs(data)
+                if res:
+                    with open("credentials.json", "w") as fd_cr:
+                        fd_cr.write(json.dumps(res))
+        except Exception as e:
+            pass
                     
 def read_logs(data):
     res = {}
